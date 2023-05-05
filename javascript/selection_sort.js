f654f8be779f39bde8235fabdf5e170842a6f29a
Historia Montague <historia.montague@outlook.com>
2023-05-05 17:24:21 -0600

    added in place selection sort algo and testing function

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,7 +1,29 @@
 function selectionSort(arr) {
   // type your code here
+  for(let i = 0; i < arr.length; i++) {
+    const lowVal = arr.findIndex((val) => val === Math.min(...arr.slice(i)))
+    if(i !== lowVal){
+      [arr[i], arr[lowVal]] = [arr[lowVal], arr[i]]
+    }
+  }
+  return arr
 }
 
+function test(testArr) {
+  let testResults = {}
+  let testNum = 0
+  testArr.forEach(test => {
+    const longInput = [];
+    for (let i = 0; i < test; ++i) {
+      longInput.push(Math.random());
+    }
+    const startTime = Date.now()
+    selectionSort(longInput)
+    testResults = {...testResults, [testNum]: {num: test,duration:(Date.now() - startTime)/1000}}
+    testNum++
+  })
+  console.log(testResults)
+}
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
@@ -10,14 +32,25 @@ if (require.main === module) {
   console.log("");
 
   // BENCHMARK HERE, and print the average runtime
-  const longInput = [];
 
-  for (let i = 0; i < 100; ++i) {
-    longInput.push(Math.random());
+  const runTimes = [3, 10, 100]
+
+  for(let i = 1; i <= 19; i++) {
+    runTimes.push((i * 100) + 100)
   }
+
+  console.log(runTimes)
+  test(runTimes)
 }
 
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
+
+//find the lowest value in the array
+//set a temp index that stores where to put the value at 
+//swap the lowest value into the temp index
+//increment the index by 1
+
+
 // And a written explanation of your solution
