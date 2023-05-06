05172cbd09bdb1357738f27c422afb891906cdae
Historia Montague <historia.montague@outlook.com>
2023-05-05 19:47:17 -0600

    Made a 2nd version of the selection sort that does not use built in methods

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,24 +1,48 @@
+//selection sort using built in methods for finding the lowest value
+//DO NOT USE THIS IT IS INCREDIBLY SLOW AND WAS A PROOF OF CONCEPT
+// function selectionSort(arr) {
+//   // type your code here
+//   for(let i = 0; i < arr.length; i++) {
+//     const lowVal = arr.findIndex((val) => val === Math.min(...arr.slice(i)))
+//     if(i !== lowVal){
+//       [arr[i], arr[lowVal]] = [arr[lowVal], arr[i]]
+//     }
+//   }
+//   console.log(arr)
+//   return arr
+// }
+
+//selection sort iterating over the array twice
 function selectionSort(arr) {
-  // type your code here
+  //set a temp lowest value index
+  let j = 0;
+  //iterate over the length of the array
   for(let i = 0; i < arr.length; i++) {
-    const lowVal = arr.findIndex((val) => val === Math.min(...arr.slice(i)))
-    if(i !== lowVal){
-      [arr[i], arr[lowVal]] = [arr[lowVal], arr[i]]
+    //iterate over the array between the current value of i and the length of the array
+    for(let n = i; n < arr.length; n++) {
+      //check if the value at a given position n is lower than the value currently at the temp index
+      if(arr[n] < arr[j]) {
+        j = n
+      }
     }
+    //after every iteration swap the values at i and the lowest value at j
+    [arr[i], arr[j]] = [arr[j], arr[i]]
   }
+  console.dir(arr, {'maxArrayLength': null});
+  console.log(arr)
   return arr
 }
 
-function test(testArr) {
+function test(testArr, sortMethod) {
   let testResults = {}
   let testNum = 0
   testArr.forEach(test => {
     const longInput = [];
     for (let i = 0; i < test; ++i) {
-      longInput.push(Math.random());
+      longInput.push(Math.floor(Math.random() * 1000));
     }
     const startTime = Date.now()
-    selectionSort(longInput)
+    sortMethod(longInput)
     testResults = {...testResults, [testNum]: {num: test,duration:(Date.now() - startTime)/1000}}
     testNum++
   })
@@ -26,10 +50,10 @@ function test(testArr) {
 }
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSort([3, -1, 5, 2]));
+  // console.log("Expecting: [-1, 2, 3, 5]");
+  // console.log("=>", selectionSort([3, -1, 5, 2]));
 
-  console.log("");
+  // console.log("");
 
   // BENCHMARK HERE, and print the average runtime
 
@@ -39,8 +63,7 @@ if (require.main === module) {
     runTimes.push((i * 100) + 100)
   }
 
-  console.log(runTimes)
-  test(runTimes)
+  test(runTimes, selectionSort)
 }
 
 module.exports = selectionSort;
