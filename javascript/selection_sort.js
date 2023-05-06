1907587fa7d0257d013ae48b25d6a2d6b1be2215
Historia Montague <historia.montague@outlook.com>
2023-05-05 20:15:20 -0600

    fixed an issue where some arrays would not be sorted properly

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,5 @@
 //selection sort using built in methods for finding the lowest value
-//DO NOT USE THIS IT IS INCREDIBLY SLOW AND WAS A PROOF OF CONCEPT
+//DO NOT USE THIS IT IS INCREDIBLY SLOW
 // function selectionSort(arr) {
 //   // type your code here
 //   for(let i = 0; i < arr.length; i++) {
@@ -14,10 +14,10 @@
 
 //selection sort iterating over the array twice
 function selectionSort(arr) {
-  //set a temp lowest value index
-  let j = 0;
   //iterate over the length of the array
   for(let i = 0; i < arr.length; i++) {
+    //set a temp lowest value index
+    let j = i;
     //iterate over the array between the current value of i and the length of the array
     for(let n = i; n < arr.length; n++) {
       //check if the value at a given position n is lower than the value currently at the temp index
@@ -28,42 +28,39 @@ function selectionSort(arr) {
     //after every iteration swap the values at i and the lowest value at j
     [arr[i], arr[j]] = [arr[j], arr[i]]
   }
-  console.dir(arr, {'maxArrayLength': null});
-  console.log(arr)
   return arr
 }
 
-function test(testArr, sortMethod) {
-  let testResults = {}
-  let testNum = 0
-  testArr.forEach(test => {
-    const longInput = [];
-    for (let i = 0; i < test; ++i) {
-      longInput.push(Math.floor(Math.random() * 1000));
-    }
-    const startTime = Date.now()
-    sortMethod(longInput)
-    testResults = {...testResults, [testNum]: {num: test,duration:(Date.now() - startTime)/1000}}
-    testNum++
-  })
-  console.log(testResults)
-}
+//function to run tests on assorted array lengths, takes in the sorting function and array containing how large you want each array passed into the sorting function to be
+
+// function test(testArr, sortMethod) {
+//   let testResults = {}
+//   let testNum = 0
+//   testArr.forEach(test => {
+//     const longInput = [];
+//     for (let i = 0; i < test; ++i) {
+//       longInput.push(Math.floor(Math.random() * 1000));
+//     }
+//     const startTime = Date.now()
+//     sortMethod(longInput)
+//     testResults = {...testResults, [testNum]: {num: test,duration:(DBate.now() - startTime)/1000}}
+//     testNum++
+//   })
+//   console.log(testResults)
+// }
+
+
 if (require.main === module) {
   // add your own tests in here
-  // console.log("Expecting: [-1, 2, 3, 5]");
-  // console.log("=>", selectionSort([3, -1, 5, 2]));
-
-  // console.log("");
-
   // BENCHMARK HERE, and print the average runtime
 
-  const runTimes = [3, 10, 100]
+  // const runTimes = [3, 10, 100]
 
-  for(let i = 1; i <= 19; i++) {
-    runTimes.push((i * 100) + 100)
-  }
+  // for(let i = 1; i <= 19; i++) {
+  //   runTimes.push((i * 100) + 100)
+  // }
 
-  test(runTimes, selectionSort)
+  // test(runTimes, selectionSort)
 }
 
 module.exports = selectionSort;
